0fbd4dde8c175c3dc6ad3b155bbade93f24e3905
Natalya <m_d.r.lecter@protonmail.com>
2022-08-17 18:51:00 +0400

    js for accordion

---
+++ js/module/faq.js
@@ -0,0 +1,60 @@
+const hide = (elem, answer) => {
+    if(!elem.classList.contains('faq__item_show')) return;
+
+    answer.style.height = `${answer.offsetHeight}px`;
+    answer.offsetHeight;
+    answer.style.display = 'block';
+    answer.style.height = 0;
+    answer.style.overflow = 'hidden';
+    answer.style.transition = 'height 0.36s ease-in-out';
+    elem.classList.remove('faq__item_show');
+
+    setTimeout(() => {
+        answer.style.display = '';
+        answer.style.height = '';
+        answer.style.overflow = '';
+        answer.style.transition = '';
+    }, 360)
+}
+
+const show = (elem, answer) => {
+    if(elem.classList.contains('faq__item_show')) return;
+
+    answer.style.display = 'block';
+    const height = answer.offsetHeight;
+    answer.style.height = 0;
+    answer.style.overflow = 'hidden';
+    answer.style.transition = 'height 0.36s ease-in-out';
+    answer.offsetHeight;
+    answer.style.height = `${height}px`;
+
+    setTimeout(() => {
+        elem.classList.add('faq__item_show');
+        answer.style.display = '';
+        answer.style.height = '';
+        answer.style.overflow = '';
+        answer.style.transition = '';
+    }, 360)
+}
+
+export const accordion = () => {
+    const list = document.querySelector('.faq__list');
+    const faqItems = list.querySelectorAll('.faq__item');
+
+    list.addEventListener('click', e => {
+        const button = e.target.closest('.faq__question');
+
+        if(button) {
+            const item = button.closest('.faq__item');
+
+            faqItems.forEach((faqItem, i) => {
+                const answer = faqItem.querySelector('.faq__answer');
+                if(item === faqItem) {
+                    faqItem.classList.contains('faq__item_show') ? hide(faqItem, answer) : show(faqItem, answer);
+                } else {
+                    hide(faqItem, answer);
+                }
+            })
+        }
+    })
+}
